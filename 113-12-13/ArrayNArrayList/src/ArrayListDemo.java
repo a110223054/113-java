54736ad9d27ef53a26ac4e93ddc93baf62002bb0
a110223054 <a110223054@mail.shu.edu.tw>
2024-12-20 11:54:06 +0800

    113-12-20

--- 113-12-13/ArrayNArrayList/src/ArrayListDemo.java
+++ 113-12-13/ArrayNArrayList/src/ArrayListDemo.java
@@ -2,11 +2,13 @@ import java.util.ArrayList;
 
 public class ArrayListDemo {
     public static void main(String[] args) {
-        ArrayList
+        ArrayList<String> list = new ArrayList<>();
 
         list.add("Apple");
         list.add("Banana");
         list.add("Cherry");
+
+        System.out.println("ArrayList內容: "+ list);
 
         String fruit = list.get(1);
         System.out.println("第二個元素是:" + fruit);
---
+++ 113-12-20/LibrarySystem_LinkList/src/person.java
@@ -0,0 +1,11 @@
+abstract public class person {
+    private String name;
+
+    public person(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
